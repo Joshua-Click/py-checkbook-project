d5cb632931c5959b35f16211c131d59b7564fa0c
Joshua-Click <joshua.click25@gmail.com>
2023-07-20 14:21:47 -0500

    updated with more comments

--- checkbook2.py
+++ checkbook2.py
@@ -2,34 +2,34 @@ import os
 
 
 
-
+# function to get the overall balance if txt file exists by adding and subtracting from the deposit or withdrawal category for the overall total
+# if txt file does not exist, it returns a balance of none
 def get_balance():
     balance = 0
 
     if os.path.exists('cb_txt.txt'):
         with open('cb_txt.txt', 'r') as f:
             for line in f:
+                #takes a transaction from deposit or withdrawal functions
                 trans = line.strip().split(',')
+                # if the transaction is 'withdraw', then the transaction is subtracted from the running balance total
                 if trans[0] == 'withdraw':
                     balance -= float(trans[1])
+                # if the transaction is "deposit", then the transaction is added fro the running balance total
                 elif trans[0] == 'deposit':
                     balance += float(trans[1])
         return balance
             
-
-    # else:
-    #     print(f'Your current balance is $ 0')
     
 
-
-# if no txt file is made return 0 balance
+# calls to the get_balance function to output the users balance
 def check_balance():
     balance = get_balance()
     print(f'Your balance is {balance}')
     
 
 
-# if no txt file is there, make one, and withdraw amount from balance
+# if txt file does not exist, this will create one and save as 'withdraw, amount'
 def withdrawal():
     amount = float(input('How much would you like to withdraw'))
     if os.path.exists('/Users/click/codeup-data-science/py-checkbook-project/cb_txt.txt'):
@@ -37,25 +37,29 @@ def withdrawal():
         with open('cb_txt.txt', 'a') as f:
             f.write(f' withdraw, {amount}\n')
     else:
-        
+        # if no txt file exists ---> gets created 
         with open('cb_txt.txt', 'w') as f:
             f.write(f' withdraw, {amount}\n')
     
 
 
-# if no txt file is there, make one, and deposit amount to balance
+# if txt file does not exist, this will create one and save as 'deposit, amount'
 def deposit():
     amount = float((input('How much?')))
 
     if os.path.exists('/Users/click/codeup-data-science/py-checkbook-project/cb_txt.txt'):
         with open('cb_txt.txt', 'a') as f:
             f.write(f'deposit,{amount}\n')
+    # if no txt file exists ---> gets created        
     else:
         with open('cb_txt.txt', 'w') as f:
             f.write(f'deposit, {amount}\n')
 
 
 
+# used for an invalid input from the choice option
+def invalid_response():
+    print('Invalid Response, choose 1 through 4')
 
 
 
@@ -63,12 +67,6 @@ def deposit():
 def exit_app():
     print('Thanks, have a great day!')
     
-
-
-
-
-
-
 
 
 
@@ -95,30 +93,21 @@ while True:
     # user input
     choice = input('Your choice?')
 
-    # will give your balance (gotta figure out how to get it to save)
-
-    # if file is there read
-    # if not balance is 0
+    # user chooses '1' and the check_balance function runs
     if (choice == '1'):
         check_balance()
     
-    
-
-
-
-    # will subtract the amount input in from the balance
+    # user chooses '2' and the withdrawal function runs
     elif (choice == '2'):
         withdrawal()
 
-
-    
-    # will add the amount input to the balance
+    # user chooses '3' and the deposit function runs
     elif (choice == '3'):
         deposit()
 
     # will give the invalid statement as per project
     elif (choice > '4'):
-        print('Invalid choice')
+        invalid_response()
         
     # will exit the app
     else: 
